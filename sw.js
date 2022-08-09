ec6f74472e79a41afa5a9a702c604347d61d33c0
lonqie <95139223+lonqie@users.noreply.github.com>
2022-08-09 11:10:15 +0100

    2022/08/09
    Added Saori
    Global Translations Update
    Updated Thai/Chinese Localization

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  */
 
 // cache versions should match common.js
-const dataCacheVer = 23;
+const dataCacheVer = 24;
 const staticCacheVer = 1;
 
 const dataCacheName = `schale-data-v${dataCacheVer}`;
